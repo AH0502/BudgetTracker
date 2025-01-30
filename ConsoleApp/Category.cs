372df2d0d3d60d5133330030cb1b4daa828538d1
Alex Hagopian <hagopianalexander@gmail.com>
2025-01-30 09:08:28 -0500

    Create constructor.

--- ConsoleApp/Category.cs
+++ ConsoleApp/Category.cs
@@ -5,6 +5,13 @@ public class Category
     public required string Name { get; set; }
     public string? Description { get; set; }
 
+    public Category(string name, string description = "")
+    {
+        if (String.IsNullOrWhiteSpace(name))
+            throw new ArgumentNullException(nameof(name), "Category name can't be null or whitespace!");
+        Name = name;
+        Description = description;
+    }
     public override string ToString() => Name;
     
 
